bf821001ce3c616e4b7097f7ee0ad8659531776f
Nordup <nordup.ondr@gmail.com>
2025-08-19 17:48:35 +0700

    remove unnecessary

--- main/server_cmd_print.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorScript
-
-func _run():
-	var godot = OS.get_executable_path()
-	var project = ProjectSettings.globalize_path("res://")
-	var cmd = "%s --path %s --server --headless" % [godot, project]
-	Debug.log_msg(cmd)
