5b9fed5d19fc57f9f44d75bbff57bf612a0aee49
Nordup <nordup.ondr@gmail.com>
2025-09-13 23:12:06 +0700

    fix button hover

--- ui/tutorial/popup_anim.gd
+++ ui/tutorial/popup_anim.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name PopupAnim
 
+@export var close: TextureButton
+
 
 func _ready():
 	if visible:
@@ -14,6 +16,7 @@ func show_popup() -> void:
 	# Fade + scale the popup in parallel
 	tween.parallel().tween_property(self, "modulate:a", 1.0, 0.2)
 	tween.parallel().tween_property(self, "scale", Vector2.ONE, 0.22).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	await tween.finished
 
 
 func hide_popup() -> void:
@@ -21,10 +24,15 @@ func hide_popup() -> void:
 	# Fade + scale the popup out in parallel
 	tween.parallel().tween_property(self, "modulate:a", 0.0, 0.2)
 	tween.parallel().tween_property(self, "scale", Vector2(0.92, 0.92), 0.2).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	await tween.finished
 	visible = false
 
 
 func show_popup_with_delay() -> void:
 	await get_tree().create_timer(0.2).timeout
 	visible = true
-	show_popup()
+	await show_popup()
+	
+	close.queue_redraw()
+	get_viewport().warp_mouse(get_viewport().get_mouse_position())
+
