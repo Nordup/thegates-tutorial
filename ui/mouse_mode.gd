fa0cd17b69d2db37dd1cfd91610594b1cf110ed8
Nordup <nordup.ondr@gmail.com>
2025-09-13 16:27:49 +0700

    don't hide mouse in world 2

--- ui/mouse_mode.gd
+++ ui/mouse_mode.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name MouseMode
 
+@export var tutorial_ui_manager: TutorialUIManager
+
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("show_mouse"): set_captured(false)
@@ -8,6 +10,9 @@ func _input(_event: InputEvent) -> void:
 
 
 func set_captured(captured: bool) -> void:
+	if tutorial_ui_manager._current_step in [2, 3]:
+		return
+
 	if captured:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	else:
